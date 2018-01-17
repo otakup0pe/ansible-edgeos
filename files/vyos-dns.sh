e3ae6cc564a01a9f8ff09ce6ef2a9f320c44e63d
Jonathan Freedman <jonafree@gmail.com>
2018-01-16 18:49:09 -0800

    crud for dns forwarders

--- files/vyos-dns.sh
+++ files/vyos-dns.sh
@@ -4,6 +4,9 @@ set -eu # you can never be trusted
 usage() {
     echo "${0} update <dns> <ip>"
     echo "${0} delete <dns>"
+    echo "${0} forward-delete <domain>"
+    echo "${0} forward-update <domain> <server>"
+    echo "${0} forward-list"
     echo "${0} list"
     exit 1
 }
@@ -18,6 +21,13 @@ if [ "$ACTION" == "update" ] || [ "$ACTION" == "delete" ] ; then
 fi
 if [ "$ACTION" = "update" ] ; then
     IP="$2"
+fi
+if [ "$ACTION" == "forward-delete" ] ; then
+    DOMAIN="$1"
+fi
+if [ "$ACTION" == "forward-update" ] ; then
+    DOMAIN="$1"
+    SERVER="$2"
 fi
 
 # These are magic scripts which come from VyOS
@@ -63,6 +73,38 @@ elif [ "$ACTION" == "list" ] ; then
         grep -B 1 inet | grep -v '\-\-' | \
         sed -e 's/^[[:space:]]*//' | cut -f 2 -d ' ' | \
         awk '{key=$0; getline; print key " - " $0;}'
+elif [ "$ACTION" == "forward-list" ] ; then
+    show service dns forwarding | grep "options server" | \
+        cut -f 2 -d '=' | sed -e 's!/!!' -e 's!/! - !'
+elif [ "$ACTION" == "forward-update" ] ; then
+    EXISTING="$(show service dns forwarding | grep "options server" | grep "$DOMAIN" | cut -f 3 -d '/')"
+    if [ ! -z "$EXISTING" ] && [ "$EXISTING" != "$SERVER" ] ; then
+        echo "Updating forward ${DOMAIN} ${EXISTING} to ${SERVER}"
+        delete service dns forwarding options "server=/${DOMAIN}/${EXISTING}"
+        set service dns forwarding options "server=/${DOMAIN}/${SERVER}"
+        commit
+        save
+    elif [ -z "$EXISTING" ] ; then
+        echo "Updating forward ${DOMAIN} to ${SERVER}"
+        set service dns forwarding options "server=/${DOMAIN}/${SERVER}"
+        commit
+        save
+    elif [ "$EXISTING" == "$SERVER" ] ; then
+        echo "Forward ${DOMAIN} is already ${SERVER}"
+    else
+        echo "Unknown state tho"
+        exit 1
+    fi
+elif [ "$ACTION" == "forward-delete" ] ; then
+    EXISTING="$(show service dns forwarding | grep "options server" | grep "$DOMAIN" | cut -f 3 -d '/')"
+    if [ ! -z "$EXISTING" ] ; then
+        echo "Deleting forward ${DOMAIN} (${EXISTING})"
+        delete service dns forwarding options "server=/${DOMAIN}/${EXISTING}"
+        commit
+        save
+    else
+        echo "Forward ${DOMAIN} already deleted"
+    fi
 else
     echo "Invalid action"
     exit 1
