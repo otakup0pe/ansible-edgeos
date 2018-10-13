e3a00d42b8a497f88bfbc1b9400002d6a62b52d5
Jonathan Freedman <jonafree@gmail.com>
2018-10-12 17:10:17 -0700

    kinda does the thing

--- files/vyos-misc.sh
+++ files/vyos-misc.sh
@@ -1,6 +1,14 @@
 #!/bin/vbash
 
 set -e
+# script-template is alias based...
+shopt -s expand_aliases
+# These are magic scripts which come from VyOS.
+# Interesting to note that this only works with `source`
+# and not `.`
+# shellcheck disable=SC1091
+source /opt/vyatta/etc/functions/script-template
+configure
 
 usage() {
     echo "${0} web restart"
@@ -10,6 +18,7 @@ usage() {
 if [ $# -lt 1 ] ; then
     usage
 fi
+
 ACTION="$1"
 shift
 
@@ -25,6 +34,13 @@ if [ "$ACTION" == "web" ] ; then
     else
         usage
     fi
+elif [ "$ACTION" == "loadkey" ] ; then
+    L_USER="$1"
+    if [ -z "$L_USER" ] ; then
+        echo "must specify username"
+        exit 1
+    fi
+    loadkey "$L_USER" "/home/${L_USER}/.ssh/authorized_keys"
 else
     usage
 fi
